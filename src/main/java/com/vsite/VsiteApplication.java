b7a25fa79491c1c0086e2a4af1cfd11483f3b900
Gylmar Gonzalez <coopervisual@hotmail.es>
2024-08-12 18:45:55 -0600

    base proyect

---
+++ src/main/java/com/vsite/VsiteApplication.java
@@ -0,0 +1,13 @@
+package com.vsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vsite/conf/RestControllerSite.java
@@ -0,0 +1,15 @@
+package com.vsite.conf;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@RequestMapping("/api/v1")
+public @interface RestControllerSite {
+
+}
